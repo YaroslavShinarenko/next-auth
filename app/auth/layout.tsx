da4d79d397c538f3daf6b4bbb715bfb5fedd8041
Yaroslav Shinkarenko <shinkarenkoyaroslav@gmail.com>
2024-02-23 21:05:19 -0600

    commit

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,6 +1,6 @@
 const DefaultAuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-lime-400 to-lime-700">
+    <div className="p-4 h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-lime-400 to-lime-700">
       {children}
     </div>
   );
--- schemas/index.ts
+++ schemas/index.ts
@@ -2,8 +2,23 @@ import * as z from "zod"
 
 export const LoginSchema = z.object({
     email: z.string({
-        invalid_type_error: "Invalid email",
-        required_error: "Email is required",
-    }).email(),
-    password: z.string()
+    }).email({
+        message: "Enter an email"
+    }),
+    password: z.string().min(6, {
+        message: "Enter a password"
+    })
+})
+
+export const RegisterSchema = z.object({
+    email: z.string({
+    }).email({
+        message: "Enter an email"
+    }),
+    password: z.string().min(6, {
+        message: "Password should be at least 6 characters"
+    }),
+    name: z.string().min(1, {
+        message: "Name is required"
+    })
 })
